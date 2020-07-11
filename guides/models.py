f287832ea4e5592f7fce4845ea531c91ff50f8db
_sagarpaudell <sp02434118@student.ku.edu.np>
2020-07-11 15:04:39 +0545

    guides model created, awaits dashboard

---
+++ guides/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from datetime import datetime
+
+class Guide(models.Model):
+    name = models.CharField(max_length=200)
+    #email = models.ForeignKey(User, on_delete= models.DO_NOTHING)
+    address = models.CharField(max_length=200)
+    city = models.CharField(max_length=100)
+    country = models.CharField(max_length=100)
+    bio = models.TextField(blank=True)    
+    contact_no = models.CharField(max_length=20)
+    gender = models.CharField(max_length=2)
+    photo_main = models.ImageField(upload_to='photos/%Y/%m/%d/')
+    photo_1 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    photo_2 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    photo_3 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    reg_date = models.DateTimeField(default=datetime.now, blank=True)
+    is_published = models.BooleanField(default=True)
+    tours_count = models.IntegerField(default=0)
+    reviews = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=3, blank=True)
+    def __str__(self):
+        return self.email
--- travellum/settings.py
+++ travellum/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'guides.apps.GuidesConfig',
     'travellers.apps.TravellersConfig',
     'pages.apps.PagesConfig',
     'django.contrib.admin',
