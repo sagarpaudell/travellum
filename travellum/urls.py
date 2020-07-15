f0830d3d7cc4aee326fbbd3ebcc94b4ef2e1c95c
Buddha69 <buddhagautam231@gmail.com>
2020-07-15 13:03:33 +0545

    created registration and login

--- travellum/urls.py
+++ travellum/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', account_views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
-    path('logout/', auth_views.LoginView.as_view(template_name='pages/index.html') ,name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='pages/index.html') ,name='logout'),
     path('profile/', account_views.profile, name='profile')
 ]
