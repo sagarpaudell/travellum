de9d531ccff439898321fc7de9cdceb9cde2f416
Biraj Adhikari <birajkmc4@gmail.com>
2020-07-16 18:46:10 +0545

    adding and changing of data from admin panel is now possible

--- travellers/admin.py
+++ travellers/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Traveller
+
+class TravellerAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'address')
+    list_display_links = ('id', 'name')
+    list_filter = ('city','country' )
+
+
+admin.site.register(Traveller, TravellerAdmin)
