af6f8a78460ef78b9473888c75da911034876894
Biraj Adhikari <birajkmc4@gmail.com>
2020-08-12 16:34:36 +0545

    Dynamic Edit Profile

--- dashboard/views.py
+++ dashboard/views.py
@@ -9,5 +9,35 @@ def dashboard(request):
   context = {
               'traveller_user':traveller_user,
             }
-  return render(request, 'dashboard/dashboard.html',context)
+  
+  if (request.method == 'POST' ):
+    root_user = User.objects.all().filter(email=user)
+    traveller_user = Traveller.objects.all().filter(email=user)
+    if 'Update Profile' in request.POST:
+      for traveller in traveller_user:
+        traveller.first_name = request.POST['first_name']
+        traveller.last_name = request.POST['last_name']
+        traveller.address = request.POST['address']
+        traveller.slogan = request.POST['slogan']
+        traveller.bio = request.POST['bio']
+        traveller.languages = request.POST['languages']
+        if traveller.is_guide:
+          traveller.price_per_hour = request.POST['pph']
+        traveller.gender = request.POST['gender']
+        
+        if request.FILES:
+          traveller.photo_main = request.FILES['photo_main']
+      
+        traveller.save()
+
+      for user in root_user:
+        user.first_name = request.POST['first_name']
+        user.last_name = request.POST['last_name']
+        user.save()
+    context = {
+              'traveller_user':traveller_user,
+               }
+    return render(request, 'dashboard/dashboard.html',context)
+
+  return render(request, 'dashboard/dashboard.html',context)  
 
--- travellers/models.py
+++ travellers/models.py
@@ -9,9 +9,11 @@ class Traveller(models.Model):
     address = models.CharField(max_length=200,blank=True)
     city = models.CharField(max_length=100,blank=True)
     country = models.CharField(max_length=100,blank=True)
-    bio = models.TextField(blank=True)    
+    bio = models.TextField(blank=True) 
+    slogan = models.CharField(max_length=1000,blank=True)   
+    languages = models.CharField(max_length=1000,blank=True)    
     contact_no = models.CharField(max_length=20,blank=True)
-    gender = models.CharField(max_length=2,blank=True)
+    gender = models.CharField(max_length=10,blank=True)
     photo_main = models.ImageField(upload_to='photos/%Y/%m/%d/',blank=True)
     photo_1 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
     photo_2 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
@@ -20,6 +22,6 @@ class Traveller(models.Model):
     is_published = models.BooleanField(default=True)
     tours_count = models. IntegerField(default=0)
     is_guide = models.BooleanField(default=False)
-    price_per_hour= models.DecimalField(max_digits=5,default=0,decimal_places = 2)
+    price_per_hour= models.DecimalField(max_digits=5,default=0,decimal_places = 0)
     def __str__(self):
         return self.first_name
