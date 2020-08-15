c2ac1d1a7e26886d19f5f29d89f834b0791121fa
Samesh <mir.rooshaan@gmail.com>
2020-08-15 13:48:33 +0545

    fixed accounts views

--- accounts/views.py
+++ accounts/views.py
@@ -18,28 +18,24 @@ def register(request):
         last_name = request.POST['last_name']
         email = request.POST['email']
         password1 = request.POST['password1']
-        password2 = request.POST['password2']
-        if password1 == password2:
             
-            if User.objects.filter(email=email).exists():
-                messages.warning(request, 'That email is being used')
-                return redirect('register')
-            else:
-                user = User.objects.create_user(
-                    email = email,
-                    first_name = first_name,
-                    last_name =last_name,
-                   
-                    password = password1
-                )
-                user.set_password(password1)
-                user.save()
-                traveller=Traveller(first_name=first_name,last_name=last_name,email=User.objects.get(email = email))
-                traveller.save()
-                return redirect('login')
-        else:
-            messages.warning(request, "Passwords doesn't match")
+        if User.objects.filter(email=email).exists():
+            messages.warning(request, 'That email is being used')
             return redirect('register')
+        else:
+            user = User.objects.create_user(
+                email = email,
+                first_name = first_name,
+                last_name =last_name,
+                
+                password = password1
+            )
+            user.set_password(password1)
+            user.save()
+            traveller=Traveller(first_name=first_name,last_name=last_name,email=User.objects.get(email = email))
+            traveller.save()
+            return redirect('login')
+
     else:
         #print(request.user.email)
         return render(request, 'accounts/register.html') 
