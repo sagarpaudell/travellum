8187b1a64d0726bfe3151e84497ce3b9313b860a
Bikesh Bimali <bimalibikesh@gmail.com>
2020-08-22 15:02:26 +0545

    Places app added back in settings and other merge errors solved

--- places/views.py
+++ places/views.py
@@ -5,16 +5,11 @@ from django.shortcuts import render,redirect
 from .models import Place
 
 def places(request):
-<<<<<<< HEAD
     return render(request, 'places/places.html') 
 
 
 def placedetails(request):
     return render(request, 'places/placedetails.html') 
-||||||| 2c21191
-    return redirect('index')
-=======
-    return redirect('index')
 
 def search(request):
     if 'term' in request.GET:  #check for source files for autocomplete 
@@ -24,4 +19,3 @@ def search(request):
             placenames.append(place.name)
         return JsonResponse(placenames, safe=False)
     return render(request, 'places/search.html')
->>>>>>> c31fb82b8706bdfa849ec0447911f2664d828b66
