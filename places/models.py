b881cc4ac3f3f6d589f17b02d6e505e6149a0e99
Biraj Adhikari <birajkmc4@gmail.com>
2020-08-25 13:27:23 +0545

    Fixed Bugs that occured While Viewing Profile

--- places/models.py
+++ places/models.py
@@ -5,10 +5,11 @@ class Place(models.Model):
     name = models.CharField(max_length=200,blank=True)
     city = models.CharField(max_length=100,blank=True)
     country = models.CharField(max_length=100,blank=True)
-    photo_1 = models.ImageField(upload_to='photos/%Y/%m/%d/',blank=True)
+    photo_1 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
     photo_2 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
     photo_3 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
     photo_4 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    description = models.TextField(blank=True)
     is_published = models.BooleanField(default=True)
     total_tours = models. IntegerField(default=0)
 
