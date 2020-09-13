ce5c2cdd4d509a71124cb23acf275c9704ee488c
Biraj Adhikari <birajkmc4@gmail.com>
2020-09-13 09:32:17 +0545

    Fixed issues with avatar in navbar

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,53 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from accounts.models import User
+from travellers.models import Traveller
+from notifications.models import Notification
 
-# Create your views here.
+
 def my_blog(request):
-    return render(request, 'blog/myBlog.html')
+    user=request.user
+    if user.is_authenticated:
+        notifications = Notification.objects.all().filter(receiver_email=user)
+        traveller_user = get_object_or_404(Traveller, email=user)
+        context = {
+                'logged_in_user':traveller_user,   #logged_in_user is for avatar in navbar
+                'notifications': notifications,
+            }
+    return render(request, 'blog/myBlog.html',context)
+
+
 def explore(request):
-    return render(request, 'blog/explore.html')
+    user=request.user
+    if user.is_authenticated:
+        notifications = Notification.objects.all().filter(receiver_email=user)
+        traveller_user = get_object_or_404(Traveller, email=user)
+        context = {
+                'logged_in_user':traveller_user,   #logged_in_user is for avatar in navbar
+                'notifications': notifications,
+            }
+    return render(request, 'blog/explore.html',context)
+
+
 def single_blog_post(request):
-    return render(request, 'blog/blogDetail.html')
+    user=request.user
+    if user.is_authenticated:
+        notifications = Notification.objects.all().filter(receiver_email=user)
+        traveller_user = get_object_or_404(Traveller, email=user)
+        context = {
+                'logged_in_user':traveller_user,   #logged_in_user is for avatar in navbar
+                'notifications': notifications,
+            }
+    return render(request, 'blog/blogDetail.html',context)
+
+
 def create_blog_post(request):
-    return render(request, 'blog/createPost.html')
+    user=request.user
+    if user.is_authenticated:
+        notifications = Notification.objects.all().filter(receiver_email=user)
+        traveller_user = get_object_or_404(Traveller, email=user)
+        context = {
+                'logged_in_user':traveller_user,   #logged_in_user is for avatar in navbar
+                'notifications': notifications,
+            }
+    return render(request, 'blog/createPost.html',context)
 
