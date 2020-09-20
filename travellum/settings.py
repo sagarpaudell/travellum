5a1762553b2f212bb12fc3851ecc1aad24d46e42
Biraj Adhikari <birajkmc4@gmail.com>
2020-09-20 23:32:23 +0545

    Esewa to confirm trip

--- travellum/settings.py
+++ travellum/settings.py
@@ -174,16 +174,16 @@ MEDIA_URL = '/media/'
 
 # esewa
 #!/usr/bin/python3
-# import requests as req
+import requests as req
 
-# url ="https://uat.esewa.com.np/epay/main"
-# d = {'amt': 100,
-#     'pdc': 0,
-#     'psc': 0,
-#     'txAmt': 0,
-#     'tAmt': 100,
-#     'pid':'ee2c3ca1-696b-4cc5-a6be-2c40d929d453',
-#     'scd':'epay_payment',
-#     'su':'http://merchant.com.np/page/esewa_payment_success?q=su',
-#     'fu':'http://merchant.com.np/page/esewa_payment_failed?q=fu'}
-# resp = req.post(url, d)
+url ="https://uat.esewa.com.np/epay/main"
+d = {'amt': 100,
+    'pdc': 0,
+    'psc': 0,
+    'txAmt': 0,
+    'tAmt': 100,
+    'pid':'ee2c3ca1-696b-4cc5-a6be-2c40d929d453',
+    'scd':'epay_payment',
+    'su':'http://merchant.com.np/page/esewa_payment_success?q=su',
+    'fu':'http://merchant.com.np/page/esewa_payment_failed?q=fu'}
+resp = req.post(url, d)
