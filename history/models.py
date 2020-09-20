f483e41d919aa7ff36879e0b4755bcae1fcc8a04
Biraj Adhikari <birajkmc4@gmail.com>
2020-09-20 09:13:13 +0545

    History Model Updated

--- history/models.py
+++ history/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from datetime import datetime
 from accounts.models import User
 from places.models import Place
+from django.utils import timezone
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 class History(models.Model):
@@ -10,5 +11,10 @@ class History(models.Model):
     place = models.ForeignKey(Place, on_delete= models.DO_NOTHING, blank=True)
     reg_date = models.DateTimeField(default=datetime.now, blank=True)
     tour_complete = models.BooleanField(default = False)
+    start_date = models.DateTimeField(blank=True)
+    end_date = models.DateTimeField(blank=True)
+    no_of_people = models.IntegerField(blank=True)
+    no_of_children = models.IntegerField(blank=True)
+    total_price = models.IntegerField(blank=True)
     def __str__(self):
         return self.traveller.first_name
