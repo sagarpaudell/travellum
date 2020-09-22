7c7f57e75a2ffd9880b3fb49c174deb909bfa6db
_sagarpaudell <sp02434118@student.ku.edu.np>
2020-09-22 22:16:04 +0545

    notifications in every page dynamic load

--- pages/views.py
+++ pages/views.py
@@ -4,6 +4,7 @@ from travellers.models import Traveller
 from places.models import Place
 from guides.models import Guide
 from accounts.models import User
+from notifications.models import Notification, Trip_Notification
 from django.shortcuts import render, redirect, get_object_or_404
  
 def index(request):
@@ -28,7 +29,20 @@ def index(request):
 def about(request):
     if request.user.is_authenticated:
         traveller_user = get_object_or_404(Traveller, email=request.user)
-    context={
-                    'logged_in_user':traveller_user,
-            }
-    return render(request, 'pages/about.html',context)
+        notifications = Notification.objects.all().filter(receiver_email=request.user).order_by('-reg_date')
+        trip_notifications = Trip_Notification.objects.all().filter(receiver_email=request.user)
+        if traveller_user.is_guide:
+            guide_user1 = get_object_or_404(Guide, email=request.user)
+            if guide_user1:
+                trip_notifications = Trip_Notification.objects.all().filter(sender_email=request.user)
+        else:
+            trip_notifications = Trip_Notification.objects.all().filter(receiver_email=request.user)
+
+        context={
+                'logged_in_user':traveller_user,
+                'notifications': notifications,
+                'trip_notifications': trip_notifications,
+                }
+        return render(request, 'pages/about.html',context)
+    return render(request, 'pages/about.html')
+
